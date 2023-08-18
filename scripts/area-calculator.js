4112a93616ec8b073b7d177da0c23d2cfea864af
MoinulHossainsaqib <moinulhossain14796@gmail.com>
2023-08-19 02:45:01 +0600

    added files and codes

---
+++ scripts/area-calculator.js
@@ -0,0 +1,49 @@
+function calculateTriangleArea(){
+    // get triangle-base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText)
+    console.log(base);
+
+    // get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // show triangle area
+
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+
+
+function calculateRectangleArea(){
+    // get rectangle width
+
+    const widthField = document.getElementById('rectangle-width')
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    // get rectangle length
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    // calculate area 
+
+    const area = width * length;
+
+    // show rectangle area
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+
+}
